68dccd5c23983ff3e1d546aa31966cb21c29c570
Brian Diolún <bdillon@live.ie>
2023-03-05 07:26:37 +0000

    starting1

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+
+    dropdown - select key
+
+
+
+    {{ first }}
+    {first}
+  </div>
+</template>
+<script>
+import { ref, computed } from "vue"
+export default {
+  name: 'IndexPage',
+    setup() {
+
+    return {
+      first: ref(1),
+
+    }
+  },
+}
+</script>
